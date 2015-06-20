1033af7319876d0f6527ccffc692fc5352e16b23
Liz <elizpar@gmail.com>
2015-06-19 21:23:54 -0400

    add a button

---
+++ scripts/scripts.js
@@ -0,0 +1,12 @@
+  
+$(document).ready(function(){
+
+
+  $('.startButton').on('mouseover', function(event) {
+    event.preventDefault();
+    alert('hello');
+  	// document.getElementById('.question1').style.display = '';
+  });
+
+
+});
