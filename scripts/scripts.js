ddcc538e96247ff0f28759a414c8a078aaad6fdc
Liz <elizpar@gmail.com>
2015-07-30 23:28:58 -0400

    assigning value to questions

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,9 +4,9 @@ $(document).ready(function(){
     function QuizGame(question, choices, qNum) {
     this.question= question;
     this.choices= choices;
-    this.qNum= qNum;
+    // this.qNum= qNum; Joe-- is this a useless bit of data now?
     }
-//this about the answers to your questions. how 
+     
     var questions= [
     new QuizGame("How much time do you have?", ["long weekend", "one week", "sabbatical"], 0),
     new QuizGame("How sick of work/stressed out are you?", ["a little", "normal", "I live in NYC/and/or work at a start-up"], 1),
@@ -14,8 +14,6 @@ $(document).ready(function(){
     new QuizGame("Do you like chaos? Choose your blend of chaos & serenity.", ["chaos", "serenity", "in the middle"], 3),
     new QuizGame("Do you like people?", ["yeah", "some", "I regularly commute on a rush hour subway/freeway."], 4)
     ];
-
-    //each choice needs to have a value in order to tally a score up to deliver answer
 
     $('.startButton').on('click', function(event) {
         event.preventDefault();
@@ -36,27 +34,43 @@ $(document).ready(function(){
 
 //on click of answer, if question number is less than total number of questions, removes old question set and replaces with next question set
 
-on click {
-if (currentquestion < 5) {
-    this.remove // removes the question
-    displayQuestion(currentQuestion); // displays the new question
-    answerQuestion(); //advances to new question
-    //store answer value
-}
-else {
-    Display calculating...
-    if {
-    
+    on click {
+    if (currentquestion < 5) {
+        this.remove // removes the question
+        displayQuestion(currentQuestion); // displays the new question
+        answerQuestion(); //advances to new question
+        //store answer value
     }
-    //total results
-    display results
-}
+    else {
+        Display calculating...
+        if {
+        
+        }
+        //total results
+        display results
+    }
 
     //store users answer point values
+
+    var answerA = questions[index].choices[0];
+    var answerB = questions[index].choices[1];
+    var answerC = questions[index].choices[2];
+
     //add answer point values
+    var score = 0;
+    if answerA {
+        var score = score + 1
+    }
+    if answerB {
+        var score = score + 2
+    }
+    if answerC {
+        var score = score + 3
+    };
+
     //this can all be a function name of country is a variable.. 
-    switch(uservalues){
-        case (uservalues > 20) :
+    switch(score){
+        case (score > 20) :
             $('.questionsHere').append('<li><span>You should go to ' results '</span></li>');
             $('.body').addClass("colombia");
             break;
