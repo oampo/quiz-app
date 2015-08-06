8c8b28047f068def834bcd04604065e8233cf849
Liz <elizpar@gmail.com>
2015-08-06 07:24:01 -0400

    questions working

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -15,84 +15,101 @@ $(document).ready(function(){
     new QuizGame("Do you like people?", ["yeah", "some", "I regularly commute on a rush hour subway/freeway."], 4)
     ];
 
+     var displayQuestion = function(index) {
+        //show question in the right spot, function takes argument of current question
+        $('.questionsHere').append('<span>' +questions[index].question+'</span>');
+        $('.answersHere').append('<li><span>' +questions[index].choices[0]+'</span></li>');
+        $('.answersHere').append('<li><span>' +questions[index].choices[1]+'</span></li>');
+        $('.answersHere').append('<li><span>' +questions[index].choices[2]+'</span></li>');
+        }
+
+     //initializes question number
+    var currentQuestion = 0;
+        // function to increase current question by 1
+    var answerQuestion = function() {
+        currentQuestion++;
+    };
+
+    var addQuestionNum = function() {
+        $('.questionNum').append('<span> Question '+ (currentQuestion + 1) + '</span>');
+    }
+
+    var removeThings = function() {
+        $('.questionNum').html('');
+        $('.questionsHere').html('');
+        $('.answersHere').html('');
+    }
+
     $('.startButton').on('click', function(event) {
         event.preventDefault();
         $('body').addClass("changeBackground");
         $(this).remove();
-        //show question in the right spot, function takes argument of current question
-        var displayQuestion = function(index) {
-            $('.questionsHere').append('<li><span>' + questions[index].question + '</span></li>');
-        };
-   });
+        addQuestionNum();
+        displayQuestion(currentQuestion);
+        });
 
-    //initializes question number
-    var currentQuestion = 0;
-        // makes increasing the current question by one a function
-        var answerQuestion = function() {
-        currentQuestion++;
-    };
-
-//on click of answer, if question number is less than total number of questions, removes old question set and replaces with next question set
-
-    $(questions[index].choice).on('click', function(event) {
+    //on click of answer, if question number is less than total number of
+    // questions, removes old question set and replaces with next question set
+    $('.answersHere').on('click', 'li', function(event) { 
         event.preventDefault();
-        if (currentquestion < questions.length) {
-            $(this).remove(); // removes the question
+        console.log(currentQuestion);
+        if (currentQuestion < questions.length - 1) {
+            removeThings(); // removes the question
             answerQuestion(); //advances to new question
+            addQuestionNum();
             displayQuestion(currentQuestion); // displays the new question
-            //store answer value
-    }
-    else if {
-        ('.questionsHere').append('<li><span>Calculating...</span></li>');
-        
-    else {
-        
-    }
-        //total results
-        ('.questionsHere').append('<li><span>You got'+ ?? + '</span></li>');
-    }
+            // nested if here? 
+            //store users answer point values
+            $(questions.choices[0]).on('click', function(event) {
+                var answerA = questions.choices[0].val();
+            }
+            // var answerA = questions.choices[0];
+            // var answerB = questions.choices[1];
+            // var answerC = questions.choices[2];
+        }
+        //questions[index].choices
+        else {
+            removeThings();
 
-    //store users answer point values
+            //add answer point values
+            var score = 0;
 
-    var answerA = questions[index].choices[0];
-    var answerB = questions[index].choices[1];
-    var answerC = questions[index].choices[2];
-
-    //add answer point values
-    var score = 0;
-    if (answerA) {
-        var score = score + 1;
-    }
-    if (answerB) {
-        var score = score + 2;
-    }
-    if (answerC) {
-        var score = score + 3;
-    };
-
-    //this can all be a function name of country is a variable.. 
+            if (answerA) {
+                var score = score + 1;
+            }
+            if (answerB) {
+                var score = score + 2;
+            }
+            if (answerC) {
+                var score = score + 3
+            };
+            console.log(score);
+            $('.questionsHere').append('<li><span>Calculating...</span></li>');
+            $('.questionsHere').append('<li><span>You got' + '</span></li>');
+        };
+    });
 
     // low, medium, high classify scores, to use the relationship
-    if (score > 20) {
-        $('.questionsHere').append('<li><span>You should go to ' + results + '</span></li>');
-        $('.body').addClass("colombia");
-    }
-    else if (uservalues > 14 && uservalues <21) {
-        $('.questionsHere').append('<li><span>You should go to Lake Baikal, Russia.</span></li>');
-        $('.body').addClass("baikal");
-    }
-    else if (uservalues > 10 && uservalues < 14) {
-        $('.questionsHere').append('<li><span>You should go to Glacier National Park.</span></li>');
-        $('.body').addClass("baikal");
-    }
-    else if (uservalues 5 > && uservalues < 11) {
-        $('.questionsHere').append('<li><span>You should go to Glacier National Park.</span></li>');
-        $('.body').addClass("gnpark");
-    }
-    else {
-        $('.questionsHere').append('<li><span>You should go to Goa India.</span></li>');
-        $('.body').addClass("india");
 
-    }
+    // if (score == 9) {
+    //     $('.questionsHere').append('<li><span>You should go to ' + results + '</span></li>');
+    //     $('.body').addClass("colombia");
+    // }
+    // else if (score > 6 && score < 9) {
+    //     $('.questionsHere').append('<li><span>You should go to Lake Baikal, Russia.</span></li>');
+    //     $('.body').addClass("baikal");
+    // }
+    // else if (score > 3 && score < 6) {
+    //     $('.questionsHere').append('<li><span>You should go to Glacier National Park.</span></li>');
+    //     $('.body').addClass("baikal");
+    // }
+    // else if (score == 3) {
+    //     $('.questionsHere').append('<li><span>You should go to Glacier National Park.</span></li>');
+    //     $('.body').addClass("gnpark");
+    // }
+    // else {
+    //     $('.questionsHere').append('<li><span>You should go to Goa India.</span></li>');
+    //     $('.body').addClass("india");
+    // }
 
 });
