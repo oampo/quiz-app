b50a9a06314ce790f9e0fdd12c524685843608f3
Joe Turner <joe@oampo.co.uk>
2015-08-20 11:01:18 +0100

    JS refactor.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,95 +1,210 @@
-  
-$(document).ready(function(){
+var quiz = {};
 
-    function QuizGame(question, choices, qNum) {
-    this.question= question;
-    this.choices= choices;
+// Models
+// ======
+
+// Question model
+quiz.Question = function(question, answers) {
+    this.question = question;
+    this.answers = answers;
+};
+
+// Result model
+quiz.Result = function(location, maxScore) {
+    this.location = location;
+    this.maxScore = maxScore;
+};
+
+// Score model
+quiz.Score = function() {
+    this.score = 0;
+};
+
+quiz.Score.prototype.increase = function(value) {
+    this.score += value;
+};
+
+// Views
+// =====
+
+// Landing View
+quiz.LandingView = function(router) {
+    this.router = router;
+
+    this.$container = $('.landing-page-view');
+    this.$start = $('.startButton');
+    this.$start.on('click', this.onStart.bind(this));
+};
+
+quiz.LandingView.prototype.show = function() {
+    this.$container.show();
+};
+
+quiz.LandingView.prototype.hide = function() {
+    this.$container.hide();
+};
+
+quiz.LandingView.prototype.onStart = function(event) {
+    event.preventDefault();
+    this.router.route('question');
+};
+
+
+// Quesion view
+quiz.QuestionView = function(questions, score, router) {
+    this.router = router;
+    this.questions = questions;
+    this.score = score;
+
+    this.currentQuestion = 0;
+
+    this.$container = $('.question-view');
+    this.$question = $('.question');
+    this.$answers = $('.answers');
+    this.$questionNum = $('.questionNum');
+
+    this.$answers.on('click', 'li', this.onSelectAnswer.bind(this));
+};
+
+quiz.QuestionView.prototype.show = function() {
+    this.$container.show();
+    this.render();
+    $('body').addClass("changeBackground");
+};
+
+quiz.QuestionView.prototype.hide = function() {
+    this.$container.hide();
+};
+
+quiz.QuestionView.prototype.render = function() {
+    this.$question.empty();
+    this.$answers.empty();
+
+    var question = this.questions[this.currentQuestion];
+
+    this.$question.append('<span>' + question.question + '</span>');
+
+    for (var i=0; i<question.answers.length; i++) {
+        var answer = question.answers[i];
+        this.$answers.append('<li><span>' + answer +'</span></li>');
     }
-     
+
+    this.$questionNum.text(this.currentQuestion + 1);
+};
+
+quiz.QuestionView.prototype.advance = function() {
+    this.currentQuestion += 1;
+    this.render();
+};
+
+quiz.QuestionView.prototype.onSelectAnswer = function(event) {
+    event.preventDefault();
+
+    if (this.currentQuestion < this.questions.length - 1) {
+        var score = $(event.currentTarget).index();
+        this.score.increase(score);
+        this.advance();
+    }
+    else {
+        this.router.route('result');
+    }
+};
+
+// Result view
+quiz.ResultView = function(results, score, router) {
+    this.results = results;
+    this.score = score;
+    this.router = router;
+
+    this.$container = $('.result-view');
+    this.$result = $('.result');
+};
+
+quiz.ResultView.prototype.show = function() {
+    this.$container.show();
+    this.render();
+};
+
+quiz.ResultView.prototype.hide = function() {
+    this.$container.hide();
+};
+
+quiz.ResultView.prototype.render = function() {
+    for (var i=0; i<this.results.length; i++) {
+        var result = this.results[i];
+        if (this.score.score <= result.maxScore) {
+            break;
+        }
+    }
+
+    this.$result.text(result.location);
+};
+
+// Services
+// ========
+
+// Router
+quiz.Router = function() {
+    this.currentRoute = null;
+    this.routes = {};
+};
+
+quiz.Router.prototype.add = function(route, view) {
+    this.routes[route] = view;
+};
+
+quiz.Router.prototype.route = function(route) {
+    // Hide the current view
+    if (this.currentRoute) {
+        this.routes[this.currentRoute].hide();
+    }
+    // Show the new view
+    this.routes[route].show();
+    this.currentRoute = route;
+};
+
+
+$(document).ready(function() {
     var questions= [
-    new QuizGame("How much time do you have?", ["long weekend", "one week", "sabbatical"]),
-    new QuizGame("How sick of work/stressed out are you?", ["a little", "normal", "I live in NYC/and/or work at a start-up"]),
-    new QuizGame("Do you need to chill or be active? Choose your blend of chill & active.", ["busy", "hectic", "mellow"]),
-    new QuizGame("Do you like chaos? Choose your blend of chaos & serenity.", ["chaos", "serenity", "in the middle"]),
-    new QuizGame("Do you like people?", ["yeah", "some", "I regularly commute on a rush hour subway/freeway."])
+        new quiz.Question("How much time do you have?", [
+                "long weekend", "one week", "sabbatical"
+        ]),
+        new quiz.Question("How sick of work/stressed out are you?", [
+                "a little", "normal",
+                "I live in NYC/and/or work at a start-up"
+        ]),
+        new quiz.Question("Do you need to chill or be active? " +
+                          "Choose your blend of chill & active.", [
+            "busy", "hectic", "mellow"
+        ]),
+        new quiz.Question("Do you like chaos? " +
+                          "Choose your blend of chaos & serenity.", [
+            "chaos", "serenity", "in the middle"
+        ]),
+        new quiz.Question("Do you like people?", [
+            "yeah", "some",
+            "I regularly commute on a rush hour subway/freeway."
+        ])
     ];
 
-     var displayQuestion = function(index) {
-        //show question in the right spot, function takes argument of current question
-        $('.questionsHere').append('<span>' +questions[index].question+'</span>');
-        $('.answersHere').append('<li><span>' +questions[index].choices[0]+'</span></li>');
-        $('.answersHere').append('<li><span>' +questions[index].choices[1]+'</span></li>');
-        $('.answersHere').append('<li><span>' +questions[index].choices[2]+'</span></li>');
-        }
+    var results = [
+        new quiz.Result('Goa, India', 2),
+        new quiz.Result('Glacier National Park, USA', 4),
+        new quiz.Result('Lake Baikal, Russia', 6),
+        new quiz.Result('Leicester, England', 8),
+        new quiz.Result('Bogota, Colombia', 10)
+    ];
 
-    //initializes question number & increase current question by 1
-    var currentQuestion = 0;
-    var answerQuestion = function() {
-        currentQuestion++;
-    };
+    var score = new quiz.Score();
 
-    //displays question number
-    var addQuestionNum = function() {
-        $('.questionNum').append('<span> Question '+ (currentQuestion + 1) + '</span>');
-    }
-    //clear q number, question and answers
-    var removeThings = function() {
-        $('.questionNum').html('');
-        $('.questionsHere').html('');
-        $('.answersHere').html('');
-    }
-    //on start, change background, remove button, 
-    $('.startButton').on('click', function(event) {
-        event.preventDefault();
-        $('body').addClass("changeBackground");
-        $(this).remove();
-        addQuestionNum();
-        displayQuestion(currentQuestion);
-        });
+    var router = new quiz.Router();
+    var landingView = new quiz.LandingView(router);
+    var questionView = new quiz.QuestionView(questions, score, router);
+    var resultView = new quiz.ResultView(results, score, router);
 
-    var score = 0;
+    router.add('start', landingView);
+    router.add('question', questionView);
+    router.add('result', resultView);
 
-    //on click of answer, if question number is less than total number of
-    // questions, removes old question set and replaces with next question set
-    $('.answersHere').on('click', 'li', function(event) { 
-        event.preventDefault();
-        // console.log(currentQuestion);
-        if (currentQuestion < questions.length - 1) {
-            score += ($(event.currentTarget).index());
-            removeThings(); // removes the question
-            answerQuestion(); //advances to new question
-            addQuestionNum();
-            displayQuestion(currentQuestion); // displays the new question
-            console.log(score);
-        }
-
-        else {
-            removeThings();
-
-            if (score == 9) {
-                $('.questionsHere').append('<li><span>You should go to ' + results + '</span></li>');
-                $('.body').addClass("colombia");
-            }
-            else if (score > 6 && score < 9) {
-                $('.questionsHere').append('<li><span>You should go to Lake Baikal, Russia.</span></li>');
-                $('.body').addClass("baikal");
-            }
-            else if (score > 3 && score < 6) {
-                $('.questionsHere').append('<li><span>You should go to Glacier National Park.</span></li>');
-                $('.body').addClass("baikal");
-            }
-            else if (score == 3) {
-                $('.questionsHere').append('<li><span>You should go to Glacier National Park.</span></li>');
-                $('.body').addClass("gnpark");
-            }
-            else {
-                $('.questionsHere').append('<li><span>You should go to Goa India.</span></li>');
-                $('.body').addClass("india");
-            }
-           
-            console.log(score);
-            $('.questionsHere').append('<li><span>Calculating...</span></li>');
-            $('.questionsHere').append('<li><span>You got' + '</span></li>');
-        };
-    });
+    router.route('start');
 });
